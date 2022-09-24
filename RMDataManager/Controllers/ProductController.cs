6fc2adba9072200882e9bc5297d1c9c7a126d8bd
NUM1NEX <g.koszyk98@gmail.com>
2022-09-24 16:36:50 +0200

    Added Product Endpoint in API

---
+++ RMDataManager/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using RMDataManger.Library.DataAccess;
+using RMDataManger.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class ProductController : ApiController
+    {
+
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+
+            return data.GetProducts();
+        }
+    }
+}
---
+++ RMDataManger.Library/DataAccess/ProductData.cs
@@ -0,0 +1,22 @@
+﻿using RMDataManger.Library.Internal.DataAccess;
+using RMDataManger.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDataManger.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SQLDataAccess sql = new SQLDataAccess();
+
+            var output = sql.LoadData<ProductModel , dynamic>("dbo.spProduct_GetAll", new { }, "RMData");
+
+            return output;
+        }
+    }
+}
